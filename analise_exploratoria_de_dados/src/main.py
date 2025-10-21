a456fd34949bcdab11b82249a231aa2755e1c09e
Matheus da Silva <matheus2001silva@live.com>
2025-10-20 21:31:20 -0300

    feat: clean_data method created on DataBase class

--- analise_exploratoria_de_dados/src/main.py
+++ analise_exploratoria_de_dados/src/main.py
@@ -4,19 +4,28 @@ from pathlib import Path
 class DataBase():
 
     def import_file(self):
-
         script_path = Path(__file__)
-
         src_dir = script_path.parent
-
         project_root = src_dir.parent
-
         csv_file_path = project_root / 'data' / 'vgsales.csv'
-
         df = pd.read_csv(csv_file_path)
 
         return df
+    
+    def clean_data(self):
+        df = self.import_file()
+        
+        if 'Ano' not in df.columns:
+            print("Erro: A coluna 'Ano' não foi encontrada no CSV.")
+            return df 
+        
+        print(f"Quantidade de linhas antes: {len(df)}")
+        cleaned_df = df.dropna(subset=['Ano'])
+        print(f"Quantidade de linhas depois: {len(cleaned_df)}")
+        
+        # 4. Retorne o dataframe limpo
+        return cleaned_df
 
 
 file = DataBase()
-print(file.import_file())
+print(file.clean_data())
