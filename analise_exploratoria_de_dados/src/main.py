1575f475084a55920052e8aca5262208d52babaf
Matheus da Silva <matheus2001silva@live.com>
2025-10-20 22:16:21 -0300

    feat: sales_and_genre_analysis method created

--- analise_exploratoria_de_dados/src/main.py
+++ analise_exploratoria_de_dados/src/main.py
@@ -28,9 +28,21 @@ class DataBase():
         return cleaned_df
     
 class DataAnalysis():
-    def __init__(self):
+    def __init__(self, df):
+        self.df = df
         pass
 
+    def sales_and_genre_analysis(self):
+        sales = self.df.groupby('Gênero')['Vendas Globais (milhões)'].sum()
+        top_genre = sales.idxmax()
+        top_sales = sales.max()
 
+        return top_genre, top_sales
+    
 
-file = DataBase()
+db = DataBase()
+cleaned_date = db.clean_data()
+
+file = DataAnalysis(cleaned_date)
+
+file.sales_and_genre_analysis()
